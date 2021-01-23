38639bdc3dcffa0b0d037180b1486a8088802489
hsyodyssey <shanaj@connect.ust.hk>
2021-01-23 11:57:54 +0800

    test level-db in go

--- main.go
+++ main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-	"github.com/hsyodyssey/agoge/config"
-	"github.com/hsyodyssey/agoge/dao"
-	"github.com/hsyodyssey/agoge/router"
+	"fmt"
+	"log"
 
-	"gorm.io/driver/mysql"
-
-	"gorm.io/gorm"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var err error
 
 func main() {
-	config.DB, err = gorm.Open(mysql.Open(config.DbURL(config.BuildDBConfig())), &gorm.Config{})
+	// config.DB, err = gorm.Open(mysql.Open(config.DbURL(config.BuildDBConfig())), &gorm.Config{})
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// config.DB.AutoMigrate(&dao.Testtable{})
+
+	// r := router.SetupRouter()
+	// r.Run()
+
+	db, err := leveldb.OpenFile("db/test", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal("Test LevelDb Go")
+	}
+	defer db.Close()
+	iter := db.NewIterator(nil, nil)
+
+	for iter.Next() {
+		key := iter.Key()
+		value := iter.Value()
+		fmt.Printf("Key: %s | Value : %s\n", key, value)
 	}
 
-	config.DB.AutoMigrate(&dao.Testtable{})
+	iter.Release()
+	err = iter.Error()
+	if err != nil {
+		log.Fatal("Test LevelDb Go")
+	}
+}
 
-	r := router.SetupRouter()
-	r.Run()
+func initLevelDb() {
+	db, err := leveldb.OpenFile("db/test", nil)
+	if err != nil {
+		log.Fatal("Yikes!")
+	}
+	defer db.Close()
+	err = db.Put([]byte("1"), []byte("1"), nil)
+	err = db.Put([]byte("2"), []byte("2"), nil)
+	err = db.Put([]byte("3"), []byte("3"), nil)
+	db.Close()
 }
