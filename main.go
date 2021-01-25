9f728ad80c12d24237cbe43f85ea4ba4eeecdc6c
hsyodyssey <shanaj@connect.ust.hk>
2021-01-25 14:48:58 +0800

    format the code structure

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/hsyodyssey/agoge/agogedb"
-	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var err error
@@ -22,36 +18,5 @@ func main() {
 	// r := router.SetupRouter()
 	// r.Run()
 
-	db, err := leveldb.OpenFile("agogedb/test/level", nil)
-	if err != nil {
-		log.Fatal("Test LevelDb Go")
-	}
-	defer db.Close()
-	iter := db.NewIterator(nil, nil)
-
-	for iter.Next() {
-		key := iter.Key()
-		value := iter.Value()
-		fmt.Printf("Key: %s | Value : %s\n", key, value)
-	}
-
-	iter.Release()
-	err = iter.Error()
-	if err != nil {
-		log.Fatal("Test LevelDb Go")
-	}
-
 	agogedb.TestRocksDB()
-}
-
-func initLevelDb() {
-	db, err := leveldb.OpenFile("agogedb/test/level", nil)
-	if err != nil {
-		log.Fatal("Yikes!")
-	}
-	defer db.Close()
-	err = db.Put([]byte("1"), []byte("1"), nil)
-	err = db.Put([]byte("2"), []byte("2"), nil)
-	err = db.Put([]byte("3"), []byte("3"), nil)
-	db.Close()
 }
