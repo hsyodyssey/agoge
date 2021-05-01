611661722c8dc929e796f02655363dfe756d94a0
hsyodyssey <shanaj@connect.ust.hk>
2021-05-02 00:10:53 +0800

    updated

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/hsyodyssey/agoge/agogedb"
-)
+import "github.com/hsyodyssey/agoge/router"
 
 func main() {
-	// agogedb.TestLevelDB()
 
-	db := agogedb.InitLevelDB()
-	iter := db.NewIterator(nil, nil)
+	r := router.SetupRouter()
+	r.Run()
 
-	for iter.Next() {
-		key := iter.Key()
-		value := iter.Value()
-		t := time.Now()
-		fmt.Printf("[LevelDB] [%d-%02d-%02d %02d:%02d:%02d] Key: %s | Value : %s\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), key, value)
-	}
-
-	iter.Release()
-	db.Close()
-	// agogedb.TestRocksDB()
 }
