e68457462e4d3959b70614dd356ec5b73e2ae838
hsyodyssey <shanaj@connect.ust.hk>
2021-05-01 22:54:41 +0800

    update leveldb

--- agogedb/leveldb.go
+++ agogedb/leveldb.go
@@ -3,6 +3,7 @@ package agogedb
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
@@ -15,10 +16,20 @@ func TestLevelDB() {
 	defer db.Close()
 	iter := db.NewIterator(nil, nil)
 
+	err = db.Put([]byte("1"), []byte("1"), nil)
+	err = db.Put([]byte("2"), []byte("2"), nil)
+	err = db.Put([]byte("3"), []byte("3"), nil)
+	err = db.Put([]byte("hsy"), []byte(""), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for iter.Next() {
 		key := iter.Key()
 		value := iter.Value()
-		fmt.Printf("Key: %s | Value : %s\n", key, value)
+		t := time.Now()
+		fmt.Printf("[LevelDB] [%d-%02d-%02d %02d:%02d:%02d 00:00] Key: %s | Value : %s\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), key, value)
 	}
 
 	iter.Release()
