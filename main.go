2b7dbcf06750b51e2a466cad6c2f5cf0979dc2f6
hsyodyssey <shanaj@connect.ust.hk>
2021-05-03 22:42:42 +0800

    updated

--- main.go
+++ main.go
@@ -2,24 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/hsyodyssey/agoge/router"
+	"github.com/urfave/cli"
 )
 
 func main() {
 
-	switch os.Args[1] {
-	case "p":
-		r := router.SetupRouter()
-		r.Run()
-	case "h":
-		fmt.Println("[Agoge] Please type p for starting...")
-		fmt.Println("[Agoge] Program stop...")
-	default:
-		fmt.Println("[Agoge] Wrong input...")
-		fmt.Println("[Agoge] Program stop...")
+	app := cli.NewApp()
 
+	// Set Flags
+	// Test    env: --env test[default]
+	// Product env: --env product
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:     "env",
+			Value:    "test",
+			Usage:    "Start the Gin server",
+			Required: true,
+		},
 	}
+	app.Action = func(c *cli.Context) error {
+		if c.String("env") == "product" {
+			// TODO develop the product version
+			fmt.Println("The product version is not avaliable yet")
+		} else if c.String("env") == "test" {
+			fmt.Println("The Test env is Staring")
+			r := router.SetupRouter()
+			r.Run()
+		} else {
+			fmt.Println("Please input the right flags [like: test or product]")
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// switch os.Args[1] {
+	// case "p":
+	// 	r := router.SetupRouter()
+	// 	r.Run()
+	// case "h":
+	// 	fmt.Println("[Agoge] Please type p for starting...")
+	// 	fmt.Println("[Agoge] Program stop...")
+	// default:
+	// 	fmt.Println("[Agoge] Wrong input...")
+	// 	fmt.Println("[Agoge] Program stop...")
+
+	// }
 
 }
