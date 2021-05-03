b2f89ea0a7a5581ce23068affd362b7df4ff1822
hsyodyssey <shanaj@connect.ust.hk>
2021-05-03 22:45:41 +0800

    deploy the cli version

--- main.go
+++ main.go
@@ -43,17 +43,4 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// switch os.Args[1] {
-	// case "p":
-	// 	r := router.SetupRouter()
-	// 	r.Run()
-	// case "h":
-	// 	fmt.Println("[Agoge] Please type p for starting...")
-	// 	fmt.Println("[Agoge] Program stop...")
-	// default:
-	// 	fmt.Println("[Agoge] Wrong input...")
-	// 	fmt.Println("[Agoge] Program stop...")
-
-	// }
-
 }
