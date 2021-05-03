2bd89227b915b2d767b99511948c13a20da356d6
hsyodyssey <shanaj@connect.ust.hk>
2021-05-03 23:55:20 +0800

    updated

--- main.go
+++ main.go
@@ -26,12 +26,15 @@ func main() {
 	}
 	app.Action = func(c *cli.Context) error {
 		if c.String("env") == "product" {
-			// TODO develop the product version
-			fmt.Println("The product version is not avaliable yet")
-		} else if c.String("env") == "test" {
-			fmt.Println("The Test env is Staring")
+			fmt.Println("[AGOGE] The product env is Staring...")
+			fmt.Println("[AGOGE] Listening on 8080...")
 			r := router.SetupRouter()
-			r.Run()
+			r.Run(":8080")
+		} else if c.String("env") == "test" {
+			fmt.Println("[AGOGE-TEST] The Test env is Staring...")
+			fmt.Println("[AGOGE-TEST] Listening on 8000...")
+			r := router.SetupRouter()
+			r.Run(":8000")
 		} else {
 			fmt.Println("Please input the right flags [like: test or product]")
 		}
